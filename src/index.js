6508d8cf2974ed7b3a938adb4919949f756683cf
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-04-27 22:04:42 +0800

    s_twart

---
+++ src/index.js
@@ -0,0 +1,22 @@
+class GameCanvas {
+    #canvas;
+    size;
+    constructor (name) {
+        this.name = name;
+        this.#canvas = document.createElement("canvas");
+        this.#canvas.setAttribute("id", name);
+    }
+    setSize(width,height) {
+        this.#canvas.width = width;
+        this.#canvas.height = height;        
+    }
+    mount() {
+        document.body.appendChild(this.#canvas);
+    }
+}
+
+export function createCanvas(name) {
+    const canvas = new GameCanvas(name);
+    canvas.mount();
+    return canvas;
+}
