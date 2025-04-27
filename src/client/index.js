7e7a8f28f177db62fb5214138fa7dc85b357acb7
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-04-27 23:01:39 +0800

    add simple server.

---
+++ src/client/index.js
@@ -0,0 +1,1 @@
+export { createCanvas } from "./Canvas.js";
--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-class GameCanvas {
-    #canvas;
-    size;
-    constructor (name) {
-        this.name = name;
-        this.#canvas = document.createElement("canvas");
-        this.#canvas.setAttribute("id", name);
-    }
-    setSize(width,height) {
-        this.#canvas.width = width;
-        this.#canvas.height = height;        
-    }
-    mount() {
-        document.body.appendChild(this.#canvas);
-    }
-}
+import { createServer } from "http";
+import fs from "fs";
+import path from "path";
 
-export function createCanvas(name) {
-    const canvas = new GameCanvas(name);
-    canvas.mount();
-    return canvas;
-}
+const MIMETYPES = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.ico': 'image/x-icon',
+};
+const publicDir = path.join(process.cwd(), "public");
+const clientDir = path.join(process.cwd(), "client");
+const server = createServer((req, res) => {
+  // server files
+  const requestedPath = req.url === '/' ? '/index.html' : req.url;
+  const [_, clientPath] = req.url.split("/client");
+  const dir = clientPath ? clientDir: publicDir;
+  const target = clientPath ? clientPath === "/" ? "index.js": clientPath : requestedPath;
+  const filePath = path.join(dir, target);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) res.writeHead(404), res.end('Not Found');
+    else res.writeHead(200, { 'Content-Type': MIMETYPES[ext] || 'text/plain' }), res.end(data);
+  });
+});
+server.listen(6565, () => console.log("WEEP http://localhost:6565"));
